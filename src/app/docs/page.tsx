7b9da7c8c57e97680bf57d7e20e709295f1cca52
sajjad <saj.maleki@gmail.com>
2025-08-18 23:13:31 +0330

    initial commit

---
+++ src/app/docs/page.tsx
@@ -0,0 +1,9 @@
+
+export default function DocsPage() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-50 to-blue-50">
+      react foam docs
+    </div>
+  );
+}
+
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-50 to-blue-50">
+      React foam home page
+    </div>
+  );
+}
